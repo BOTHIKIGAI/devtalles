c95085c45ebb64320de6d90ef18f08b63b5caa46
BOTHIKIGAI <estebax89@gmail.com>
2024-06-13 09:14:34 -0500

    save point

--- programacion_para_principiantes/hanging/src/App.tsx
+++ programacion_para_principiantes/hanging/src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import { HangImage } from './components/HangImage';
+import { letters } from './helpers/letters';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [word] = useState('COMPUTADORA');
+  const [hiddenWord, setHiddenWord] = useState('_ '.repeat(word.length));
+  const [attemps, setAttemps] = useState(0);
+
+  const checkLetter = (letter: string) => {
+
+    if (!word.includes(letter)) {
+      setAttemps(Math.min(attemps + 1, 9));
+      return;
+    }
+
+    const hiddenWordArray = hiddenWord.split(' ');
+    
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === letter) {
+        hiddenWordArray[i] = letter;
+      }
+    }
+
+    setHiddenWord(hiddenWordArray.join(' '));
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        {/* Imagenes */}
+        <HangImage imageNumber={attemps} />
+
+        {/* Palabra oculta */}
+        <h3>{hiddenWord}</h3>
+
+        {/* Contador de intentos */}
+        <h3>Intentos: {attemps}</h3>
+
+        {/* Botones de letras */}
+        {
+          letters.map((letter) => (
+            <button
+              onClick={() => checkLetter(letter)}
+              key={letter}
+            >
+              {letter}
+            </button>
+          ))
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
-}
 
-export default App
+};
+
+export default App;
--- programacion_para_principiantes/hanging/src/main.tsx
+++ programacion_para_principiantes/hanging/src/main.tsx
@@ -1,10 +1,12 @@
+// Todo el archivo es un JSX
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> {/* StricMode para revisar errores */}
+    <App /> {/* Etiqueta que nosotros creamos, explicando que se desea renderizar  */}
   </React.StrictMode>,
 )
