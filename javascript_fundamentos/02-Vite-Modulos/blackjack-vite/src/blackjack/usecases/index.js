2b50106012bed52009be0853c41c5e703ea7922f
BOTHIKIGAI <estebax89@gmail.com>
2024-07-29 22:05:16 -0500

    finalización modularización

--- javascript_fundamentos/02-Vite-Modulos/blackjack-vite/src/blackjack/usecases/index.js
+++ javascript_fundamentos/02-Vite-Modulos/blackjack-vite/src/blackjack/usecases/index.js
@@ -1,3 +1,5 @@
+export { crearCartaHTML } from './crear-carta'
 export { crearDeck as createNewDeck } from "./crear-deck";
 export { pedirCarta } from './take-card';
+export { turnoComputadora } from './turno-computadora'
 export { valorCarta } from './value-card';
