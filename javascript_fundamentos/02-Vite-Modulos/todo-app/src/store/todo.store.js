e9d8c21c44e07d35075e42e574b09e8de754edd2
BOTHIKIGAI <estebax89@gmail.com>
2024-07-31 21:11:00 -0500

    construccion logica de metodos de todo

--- javascript_fundamentos/02-Vite-Modulos/todo-app/src/store/todo.store.js
+++ javascript_fundamentos/02-Vite-Modulos/todo-app/src/store/todo.store.js
@@ -20,6 +20,85 @@ const initStore = () => {
     console.log('InitStore 🥔');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented')
+}
+
+
+/**
+ * @param {String} filter 
+ */
+const getTodos = ( filter = Filters.All) => {
+
+    switch ( filter ) {
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done )
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done )
+        default:
+            throw new Error(`Option ${filter} is not valid.`)
+    }
+
+}
+
+
+/**
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+
+    if ( !description ) throw new Error ('Descripción is required')
+    state.todos.push( new Todo(description) )
+
+}
+
+
+/**
+ * @param {String} todoId 
+ */
+const toggleTodo = ( todoId ) => {
+    throw new Error('Not implemented')
+}
+
+
+/**
+ * @param {String} todoId 
+ */
+const deleteTodo = ( todoId ) => {
+    state.todos = state.todos.filter( todo => todo.id !== todoId )
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter( todo => todo.done )
+}
+
+
+/**
+ * Función para crear un filtro
+ * @param {Filters} newFilter 
+ */
+const setFilter = ( newFilter = Filters.All ) => {
+    state.filter = newFilter;
+}
+
+
+/**
+ * Funcion para traer los filtros
+ */
+const getCurrentFilter = () => {
+    return state.filter.toString();
+}
+
 export default {
-    initStore
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
+    initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
