ca0772f19336b90d0d0a542f718e6184244ba16c
BOTHIKIGAI <estebax89@gmail.com>
2024-08-04 19:44:57 -0500

    callback hell

--- javascript_fundamentos/03-Callbacks/06-advance/src/concepts/02-callbacks.js
+++ javascript_fundamentos/03-Callbacks/06-advance/src/concepts/02-callbacks.js
@@ -6,16 +6,25 @@ import { heroes } from '../data/heroes'
  */
 export const callbacksComponent = ( element ) => {
 
-    const id = '5d86371fd55e2e2a30fe1ccb1'
-    findHero( id, (error, hero) => {
+    const id1 = '5d86371fd55e2e2a30fe1ccb1'
+    const id2 = '5d86371fd55e2e2a30fe1ccb2'
+
+    findHero( id1, (error, hero1) => {
         // element.innerHTML = hero?.name || 'No hay heroe'
         
         if ( error ) {
             element.innerHTML = error
             return
         }
+
+        findHero( id2, (error, hero2) => {
+            if ( error ) {
+                element.innerHTML = error
+                return
+            }
+            element.innerHTML = `${hero1.name} / ${hero2.name}`
+        })
         
-        element.innerHTML = hero.name
     })
 
 }
